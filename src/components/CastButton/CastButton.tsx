9f483e808de2d5e166c90e5e11ffa6ea9733430c
Wes Guirra <wesleyguirra@users.noreply.github.com>
2023-03-03 00:25:10 -0300

    Update CastButton.tsx

--- src/components/CastButton/CastButton.tsx
+++ src/components/CastButton/CastButton.tsx
@@ -1,14 +1,6 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 import * as React from 'react';
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'google-cast-launcher': any;
-    }
-  }
-}
-
 const CastButton = ({ ...props }) => {
   return <google-cast-launcher style={{ cursor: 'pointer' }} {...props} />;
 };
